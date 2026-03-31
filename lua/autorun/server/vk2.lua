8dae716e558c787fb08b21322575faef3768ed6a
ktypt <admin@example.com>
2026-03-31 16:05:18 +0500

    Add license and copyright

--- lua/autorun/server/vk2.lua
+++ lua/autorun/server/vk2.lua
@@ -1,3 +1,18 @@
+-- vk2.lua
+-- Copyright (C) 2018-2026 ktypt, selyodka, kot, akych
+
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 module( "kotLib.vk", package.seeall )
 util.AddNetworkString( "Test2" )
 net.Receive( "Test2", function(len, ply)  
